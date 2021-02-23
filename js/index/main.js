11a53a963e2490bfd982e87d4c75cd8e88dccce6
Evan <evan40907870@gmail.com>
2021-02-23 19:14:18 +0800

    .

---
+++ js/index/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.IamSajid').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+    });
+});
+$(document).ready(function(){
+  $('.IamSajid123').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+});
+});
